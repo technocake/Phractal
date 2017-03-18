52455838de91ff6b13a8e2944b049c38498a6432
technocake <technocake@komsys.org>
2017-03-19 00:21:18 +0100

    BugOrFeature

--- animator.py
+++ animator.py
@@ -3,17 +3,32 @@
 
 
 import turtle
+from turtle import *
 import time
 import math
 
-def drawPhree():
+
+def drawPhree(x, y, d, p=90, n=1):
 	""" a simple start """
-	turtle.seth(90)
-	turtle.forward(150)
+	branch = Turtle()
+	branch.goto(x,y)
+	
+	if (n==1):
+		branch.seth(90)
+		branch.forward(d)
+	else:
+		branch.seth(p)	
+		branch.forward(d)
+	
+	if (n < 10):
+		x,y = branch.pos()
+		drawPhree(x,y,d, 60, n+1)
+		drawPhree(x,y,d, 120, n+1)
+
 
 
 if __name__ == '__main__':
-	drawPhree()
+	drawPhree(x=0,y=0,d=100)
 
 	turtle.listen()
 	turtle.mainloop()
