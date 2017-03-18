29320c174c58d186a19fe9fa4394b1ca30789598
technocake <technocake@komsys.org>
2017-03-18 23:57:33 +0100

    Small start

---
+++ animator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import turtle
+import time
+import math
+
+def drawPhree():
+	""" a simple start """
+	turtle.seth(90)
+	turtle.forward(150)
+
+
+if __name__ == '__main__':
+	drawPhree()
+
+	turtle.listen()
+	turtle.mainloop()
