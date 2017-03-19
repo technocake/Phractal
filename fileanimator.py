46d2b9a7e820542a51a6deac9e620ef3f69d2003
Jonas Solsvik <jonas.solsvik@gmail.com>
2017-03-19 01:05:10 +0100

    Found root node from file

--- fileanimator.py
+++ fileanimator.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+
+with open("file/filformat_alpha.tree", "r") as treefile:
+
+  for line in treefile.readlines():
+    
+    if line.find("node: 0") > -1:
+      print(line)
+
+    elif line.find("node:"):
+      pass
