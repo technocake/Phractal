427f1eaa15c0e3bc7c814c4223db33fbcf436456
Jonas Solsvik <jonas.solsvik@gmail.com>
2017-03-19 21:52:57 +0100

    New file graph_animator.py

---
+++ graph_animator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 
+# @author  Jonas Solsvik
+# @email   jonasjso@stud.ntnu.no
+# @created 18.03.17
+# @file    graph_animator.py
+# @brief   Takes fileformat.graph kind of files and draws
+#           a graph based, layer by layer.
