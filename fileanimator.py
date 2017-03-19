799f398bba4e0f17152313c704db333f8536ab7d
Jonas Solsvik <jonas.solsvik@gmail.com>
2017-03-19 02:45:35 +0100

    Changed logical datastructure to JSON-like

--- fileanimator.py
+++ fileanimator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-tree=[]
+
 
 def serialize_line(line):
   line = line.split(":")[1]      \
@@ -9,6 +9,23 @@ def serialize_line(line):
                .replace("\n", "") \
                 .split(",")
   return line
+
+
+def serialize_layer(line):
+  line = serialize_line(line)
+  return dict({
+      "layer_id": line[0],
+      "number_of_bases": line[1],
+  })
+
+
+def serialize_base(line):
+  line = serialize_line(line)
+  return dict({
+      "base_id": line[0],
+      "number_of_nodes": line[1],
+  })
+
 
 def serialize_node(line):
   line = serialize_line(line)
@@ -18,54 +35,56 @@ def serialize_node(line):
       "radius": line[3],   
   })
 
-def serialize_base(line):
-  line = serialize_line(line)
-  return dict({
-      "base_id": line[0],
-      "number_of_nodes": line[1],
-  })
 
 def read_file_tree():
+  tree=[]
+
   with open("file/filformat_alpha.tree", "r") as treefile:
 
     current_layer = -1
+
 
     for line in treefile.readlines():
         
       # Special case for finding the root node
       if line.find("layer:") > -1:
-        tree.append([])
         current_layer += 1
+        current_base = -1
+
+        layer = serialize_layer(line)
+        tree.append(dict({
+          "layer": layer, 
+          "bases": [], 
+        }))
 
       elif line.find("base:") > -1:
+        current_base += 1
+
         base = serialize_base(line)
-        tree[current_layer].append(base)
+        tree[current_layer]["bases"].append(dict({
+          "base" : base, 
+          "nodes" : [],
+        }))
 
       elif line.find("node:") > -1:
         node = serialize_node(line)
-        tree[current_layer].append(node)
+        tree[current_layer]["bases"][current_base]["nodes"].append(node)
         #print(node)
 
-def display_tree_data():
-  layer_count=1
+  return tree
+
+
+def display_tree_data(tree):
+
   for layer in tree:
-    counter = 0
-    print("\nLayer:", layer_count)
-    layer_count += 1
-
-    while counter < len(layer):
-      number_of_nodes = layer[counter]['number_of_nodes']
-
-      print("\n", layer[counter])
-      counter += 1
-
-      for j in range(int(number_of_nodes)):
-
-        print(layer[counter])
-        counter += 1
+    print("\n", layer["layer"])
+    for base in layer["bases"]:
+      print(base["base"])
+      for node in base["nodes"]:
+        print(node)
 
 
 if __name__ == "__main__":
-  read_file_tree()
-  display_tree_data()
+  tree = read_file_tree()
+  display_tree_data(tree)
 
