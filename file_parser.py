3b3fe763f1fb44acde4ed9a2fd32a810a3ae8aa1
Jonas Solsvik <jonas.solsvik@gmail.com>
2017-03-19 22:10:13 +0100

    Started work on graph_animator.py + lots of small changes to other files

--- file_parser.py
+++ file_parser.py
@@ -4,7 +4,7 @@
 # 
 # @author  Jonas Solsvik
 # @email   jonasjso@stud.ntnu.no
-# @created 18.03.17
+# @created 19.03.17
 # @file    file_parser.py
 # @brief   Collection of functions regarding the parsing
 #           of lines from a file
@@ -13,9 +13,53 @@
 #             *.graph
 
 
-def line_to_list(line):
+def serialize_line(line):
   line = line.split(":")[1]      \
               .replace(" ", "")  \
                .replace("\n", "") \
                 .split(",")
   return line
+
+
+def serialize_layer(line):
+  line = serialize_line(line)
+  return dict({
+      "layer_id": int(line[0]),
+      "number_of_bases": int(line[1]),
+  })
+
+
+def serialize_base(line):
+  line = serialize_line(line)
+  return dict({
+      "base_id": int(line[0]),
+      "number_of_nodes": int(line[1]),
+  })
+
+
+def serialize_node(line):
+  line = serialize_line(line)
+  return dict({ 
+      "color":  line[1],  
+      "theta":  int(line[2]),
+      "radius": int(line[3]),   
+  })
+
+
+#
+# @function that serializes lines that looks like the one below
+#           into dictionaries.
+#      @example "node: 0, 0, 0, 0, 100, 1, orange"
+#               <tag><id><base_layer><base_node><theta><radius><size><color>
+#
+def serialize_graph_node(line):
+  list_of_values = serialize_layer(line)
+  return {
+    "id"         : int(list_of_values[0]),
+    "base_layer" : int(list_of_values[1]),
+    "base_node"  : int(list_of_values[2]),
+    "theta"      : int(list_of_values[3]),
+    "radius"     : int(list_of_values[4]),
+    "size"       : int(list_of_values[5]),
+    "color"      :     list_of_values[6],
+  }
--- graph_animator.py
+++ graph_animator.py
@@ -4,7 +4,33 @@
 # 
 # @author  Jonas Solsvik
 # @email   jonasjso@stud.ntnu.no
-# @created 18.03.17
+# @created 19.03.17
 # @file    graph_animator.py
-# @brief   Takes fileformat.graph kind of files and draws
-#           a graph based, layer by layer.
+# @brief   Takes "fileformat.graph"-kind of files and draws
+#           a graph based on it, layer by layer.
+
+
+import file_parser as parser
+  
+filepath = "file/fileformat.graph" # File to load the graph from
+
+
+def read_file_tree():
+  graph=[]
+
+  with open(filepath, "r") as treefile:
+
+    current_layer = -1
+
+    for line in treefile.readlines():
+        
+      if line.find("layer:") > -1:
+        current_layer += 1
+
+      elif line.find("node:") > -1:
+        node = serialize_graph_node(line)
+        graph[current_layer].append(node)
+
+  return tree
+
+
