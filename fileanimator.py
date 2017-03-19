a6146a6f4926a7427bca1b92907cd7e877c6348f
Jonas Solsvik <jonas.solsvik@gmail.com>
2017-03-19 12:17:04 +0100

    Cleanup of turtle_dance function. More readable now

--- fileanimator.py
+++ fileanimator.py
@@ -79,7 +79,7 @@ def turtle_factory(color):
   tut.shape("circle")
   tut.shapesize(.5)
   tut.color(color)
-  tut.speed(9)
+  tut.speed(0)
   return tut
 
 def turtle_clonery(tut, color):
@@ -89,65 +89,80 @@ def turtle_clonery(tut, color):
    return newtut
 
 
-def turtles_spread(turtles, nodes):
-  opened = False
-  radis = []
 
-  # 2. Step - Make all branches face the right direction
-  i=0
-  for node in nodes:
-    if "base_id" in node:
-      continue
-    else:
-      radis.append([0, int(node["radius"])])
-      turtles[i].left(node["theta"])
-      i+=1
+def turtles_dance(tree):
 
-  # 3. Step run nodes forward
-  while not opened:
-    opened = True
-
-    i = 0
-    for rad in radis:
-      if rad[0] <= rad[1]:
-        rad[0] += 1
-        turtles[i].forward(1)
-        opened = False
-
-      i += 1
-
-
-
-def turtle_dance(tree):
   screen = Screen()
 
-    # Root turtle
-  tutroot=turtle_factory(tree[0]["nodes"][1]["color"])
-
+  #
+  # 0. step - Create root turtle
+  #
+  tutroot = turtle_factory(tree[0]["nodes"][1]["color"])
   tutroot.left(tree[0]["nodes"][1]["theta"])
   tutroot.penup()
   tutroot.forward(tree[0]["nodes"][1]["radius"])
   tutroot.pendown()
 
-  bot_layer = [tutroot]
-  top_layer = []
+  #
+  # 0.5 step - Root turtle is now the base "layer" for layer 1 of turtles
+  #
+  base_turtle = [tutroot]
 
   for layer in tree[1:]:
-    # 1. step - create new turtles
+    #
+    # 1. step - clone new turtles
+    #
+    cloned_turtle = []
     base_num = -1
     nodes = layer["nodes"]
+
     for node in nodes:
       if "base_id" in node:
         base_num += 1
         print("Base_num:", base_num)
         pass
       else:
-        top_layer.append(turtle_clonery(bot_layer[base_num], node["color"]))
+        cloned_turtle.append( turtle_clonery( base_turtle[base_num], node["color"]))
 
-    turtles_spread(top_layer, nodes)
 
-    bot_layer = top_layer # Push back top_layer
-    top_layer = []
+    #
+    # 2. Step - Make all turtles face the right direction
+    #
+    i=0
+
+    radius = []
+    node_count = len(nodes)
+
+    for node in nodes:
+      if "base_id" in node:
+        continue
+      else:
+        radius.append(int(node["radius"]))
+        cloned_turtle[i].left(node["theta"])
+        i+=1
+
+    #
+    # 3. Step by step run cloned turtles forward untill they hit
+    #     their max radius from the base.
+    #
+    forward_count = [ 0 for i in range(node_count) ]
+    opened = False
+
+    while not opened:
+      opened = True
+
+      for i in range(len(radius)):
+        if forward_count[i] <= radius[i]:
+          forward_count[i] += 1
+
+          cloned_turtle[i].forward(1)
+          opened = False
+
+    #
+    # 4. Cloned turtles are now the base layer for the next
+    #     layer of turtles
+    #
+    base_turtle = cloned_turtle 
 
   screen.mainloop()
 
@@ -155,5 +170,5 @@ def turtle_dance(tree):
 if __name__ == "__main__":
   tree = read_file_tree()
   display_tree_data(tree)
-  turtle_dance(tree)
+  turtles_dance(tree)
 
